d031db0e19324cee9abe6e11bf23ca6c4e5966f9
Ole Henrik Skogstrøm <Ole Henrik Skogstrm>
2019-01-17 11:53:54 +0100

    Fixed run for ever loop in example

--- examples/new_websocket.py
+++ examples/new_websocket.py
@@ -28,10 +28,6 @@ async def cancel_order(client, cid):
     print(cancel_message)
     return cancel_message
 
-async def continue_to_listen():
-    while True:
-        await asyncio.sleep(0.1)
-
 async def main():
     client = WssClient(
         key=os.environ.get("BITFINEX_KEY"),
@@ -41,11 +37,10 @@ async def main():
     asyncio.ensure_future(client.subscribe_to_trades(symbol="BTCUSD", callback=async_print))
     order_response = await create_order(client)
     # pylint: disable=W0612
-    cancel_order_response = await cancel_order(client, order_response["cid"])
-    await asyncio.sleep(3)
+    await cancel_order(client, order_response["cid"])
 
-    # Continue to listen for answers after cancel order completed.
-    # await continue_to_listen()
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main_loop = asyncio.get_event_loop()
+    asyncio.ensure_future(main())
+    main_loop.run_forever()
